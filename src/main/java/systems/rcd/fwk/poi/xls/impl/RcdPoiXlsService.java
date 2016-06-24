aa5917ed03f0619e9cc86122fcd33975a77ebdcb
rcd-systems <comptepoubelle8@gmail.com>
2016-06-24 23:14:39 +0200

    Removed exception declaration

--- src/main/java/systems/rcd/fwk/poi/xls/impl/RcdPoiXlsService.java
+++ src/main/java/systems/rcd/fwk/poi/xls/impl/RcdPoiXlsService.java
@@ -1,17 +1,15 @@
 package systems.rcd.fwk.poi.xls.impl;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import systems.rcd.fwk.core.ctx.RcdContext;
+import systems.rcd.fwk.core.exc.RcdException;
 import systems.rcd.fwk.core.format.xls.RcdXlsService;
 import systems.rcd.fwk.poi.xls.impl.data.RcdPoiXlsWorkbook;
 
@@ -25,10 +23,16 @@ public class RcdPoiXlsService
 
     @Override
     public RcdPoiXlsWorkbook instRead( final Path path )
-        throws EncryptedDocumentException, InvalidFormatException, IOException
     {
-        final InputStream inputStream = Files.newInputStream( path, StandardOpenOption.READ );
-        final Workbook workbook = WorkbookFactory.create( inputStream );
-        return new RcdPoiXlsWorkbook( workbook );
+        try
+        {
+            final InputStream inputStream = Files.newInputStream( path, StandardOpenOption.READ );
+            final Workbook workbook = WorkbookFactory.create( inputStream );
+            return new RcdPoiXlsWorkbook( workbook );
+        }
+        catch ( Exception e )
+        {
+            throw new RcdException( "Error while reading XLS file [" + path + "]", e );
+        }
     }
 }
